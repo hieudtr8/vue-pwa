f72a79d06229464d228435fca53042fc217511d9
Do Trong Hieu <73334639+hieudtr8@users.noreply.github.com>
2023-03-22 23:08:08 +0700

    [Fix] Custom display message service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,32 +1,46 @@
-/* eslint-disable no-console */
-
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
+    ready() {
       console.log(
         'App is being served from cache by a service worker.\n' +
         'For more details, visit https://goo.gl/AFskqB'
       )
     },
-    registered () {
+    registered() {
       console.log('Service worker has been registered.')
+      // Routinely check for app updates by testing for a new service worker.
     },
-    cached () {
+    cached() {
       console.log('Content has been cached for offline use.')
     },
-    updatefound () {
+    updatefound(registration) {
       console.log('New content is downloading.')
+      const installingWorker = registration.installing
+      console.log(installingWorker)
+
+      document.dispatchEvent(
+        new CustomEvent('swUpdateFound', { detail: registration })
+      )
     },
-    updated () {
+    updated(registration) {
       console.log('New content is available; please refresh.')
+      document.dispatchEvent(
+        new CustomEvent('swUpdated', { detail: registration })
+      )
+      // Add a custom event and dispatch it.
+      // Used to display of a 'refresh' banner following a service worker update.
+      // Set the event payload to the service worker registration object.
+
     },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+    offline() {
+      console.log(
+        'No internet connection found. App is running in offline mode.'
+      )
     },
-    error (error) {
+    error(error) {
       console.error('Error during service worker registration:', error)
-    }
+    },
   })
 }
